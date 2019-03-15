6fa2dad62b51ae61933d96191f05eaa5629e871f
GustavoLiu <gustavo_santostr@hotmail.com>
2019-03-15 20:21:24 -0300

    Primeiro Comimit

--- Django/adocao/urls.py
+++ Django/adocao/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
      # 'caminho/da/url',
      #  ClasseLáDoView.as view(),
      #   name = "nomeDessaUrl")
-     path('inicio', PaginaInicialView.as_view(), name="index"),
+     path('', PaginaInicialView.as_view(), name="index"),
      path('sobre', SobreView.as_view(), name="sobre")
 ]
